509fba919d580d578ee3a98466d515ed7098488d
Manjeet Kumar <mkj22834@gmail.com>
2026-06-06 08:58:52 +0530

    Notification model added

---
+++ backend/models/notification.model.js
@@ -0,0 +1,29 @@
+const {Schema, default: mongoose} = require("mongoose")
+
+const notificationSchema = new Schema({
+    receiver:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    // type of the notification (connection accept,like,comment)
+    type:{
+        type:String,
+        enum:["like","comment","connectionAccepted"]
+    },
+    // sender
+    relatedUser:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    // post you like and comment
+    relatedPost:{
+        type:Schema.Types.ObjectId,
+        ref:"Post"
+    },
+
+},{timestamps:true})
+
+
+const Notification = new mongoose.model("Notification",notificationSchema)
+
+module.exports = Notification
